da4b46ade11c354838bd2e8dec259c0e5701d122
yuuri0908 <148744622+yuuri0908@users.noreply.github.com>
2024-06-28 01:28:21 +0900

    Delete chatbot directory

--- chatbot/chatbot/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from  chatbotapp import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
-    path('bot_response/', views.bot_response, name='bot_response'), # 応答
-]
